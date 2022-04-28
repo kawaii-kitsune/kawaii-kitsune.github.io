978551112bc5c089481cfd752a037229bde4213b
kawaii-kitsune <72494172+kawaii-kitsune@users.noreply.github.com>
2022-04-28 10:55:32 +0300

    Add files via upload

---
+++ dist/javascript/index.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
+
+
+$(document).on('click', '.timeline-tab', function (event) {
+  let tabs=document.getElementsByClassName('timeline-tab');
+  for(let i=0;i<tabs.length;i++){
+    let tab_name=tabs[i].attributes.data_toggle.value
+    $(tab_name).css("display", "none")
+    tabs[i].classList.remove("is-active")
+  }
+  $(event.currentTarget.attributes.data_toggle.value).css("display", "block")
+  event.currentTarget.classList.add("is-active")
+});
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
